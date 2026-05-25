3237fa5ccd0f8d0f842a456faec614f196cd6993
Ayman Ali <aymanhashem0001@gmail.com>
2026-05-25 17:18:21 +0300

    designed the test_array function

--- tests/custom_tests.c
+++ tests/custom_tests.c
@@ -9,3 +9,16 @@ void test_result(int actual_result, int expected_result, char *test_name) {
 
     printf("%s \033[0;32m[PASSED]\033[0;0m\n", test_name);
 }
+
+
+void test_array(int result_arr[], int expected_arr[], size_t len, char *test_name) {
+    for (int i = 0; i < len; i++) {
+        if (result_arr[i] != expected_arr[i]) {
+            printf("%s \033[0;31m[FAILED]\033[0;0m\n", test_name);
+            printf("At index: %i -> Expected: %i | Got: %i\n", i, expected_arr[i], result_arr[i]);
+            return;
+        }
+    }
+
+    printf("%s \033[0;32m[PASSED]\033[0;0m\n", test_name);
+}
