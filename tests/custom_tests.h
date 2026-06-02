525406595cb4c03078afc7fdc746a2418ade3e05
Ayman Ali <aymanhashem0001@gmail.com>
2026-06-02 12:05:43 +0300

    added header file for custom_tests and added more tests to linear search testing

---
+++ tests/custom_tests.h
@@ -0,0 +1,30 @@
+#ifndef CUSTOMTESTS_H
+#define CUSTOMTESTS_H
+
+#include <stddef.h>
+
+/**
+ * @brief Test an integer returned by a function.
+ * 
+ * @param actual_result: The integer returned by a function.
+ * @param expected_result: The integer that is expected to be returned by the function.
+ * @param test_name: Custom name for the test to make each test unique and easy to identify.
+ * 
+ * @return Nothing (void).
+ */
+void test_result(int actual_result, int expected_result, char *test_name);
+
+/**
+ * @brief Test an array returned by a function.
+ * 
+ * @param result_arr: The array returned by a function.
+ * @param expected_arr: The array that is expected to be returned by the function.
+ * @param len: Length of both result_arr and expected_arr (both arrays must be of the same length).
+ * @param test_name: Custom name for the test to make each test unique and easy to identify.
+ * 
+ * @return Nothing (void). 
+ */
+void test_array(int result_arr[], int expected_arr[], size_t len, char *test_name);
+
+
+#endif
--- tests/testing_linear_search.c
+++ tests/testing_linear_search.c
@@ -1,21 +1,33 @@
 #include "../algorithms/my_algorithms.h"
-#include "custom_tests.c"
+#include "custom_tests.h"
 
 int main(void) {
     int arr1[8] = {42, 12, 89, 7, 23, 54, 91, 31};
     test_result(linear_search(arr1, 8, 23), 4, "Random test");
 
     int arr2[9] = {65, 8, 19, 74, 3, 99, 41, 12, 57};
-    test_result(linear_search(arr2, 9, 65), 0, "Best case");
+    test_result(linear_search(arr2, 9, 65), 0, "Best case");   // Element at the beginning
     
     int arr3[9] = {11, 83, 4, 38, 92, 67, 15, 50, 76};
-    test_result(linear_search(arr3, 9, 76), 8, "Worst case 1");
+    test_result(linear_search(arr3, 9, 76), 8, "Worst case: Element at the end");
     
     int arr4[5] = {80, 29, 57, 26, 90};
-    test_result(linear_search(arr4, 5, 13), -1, "Worst case 2");
+    test_result(linear_search(arr4, 5, 13), -1, "Worst case: Element doesn't exist");
 
     int arr5[1] = {};   // array initialize with 1 because you can't initialize an array with a zero
-    test_result(linear_search(arr5, 0, 1), -1, "Worst case 3");
+    test_result(linear_search(arr5, 0, 1), -1, "Empty array");
+
+    int arr6[1] = {42};
+    test_result(linear_search(arr6, 1, 42), 0, "Single-element match");
+
+    int arr7[1] = {7};
+    test_result(linear_search(arr7, 1, 14), -1, "Single-element mismatch");
+
+    int arr8[5] = {5, 2, 7, 5, 9};
+    test_result(linear_search(arr8, 5, 5), 0, "Array with duplicates");
+
+    int arr9[4] = {-15, 0, 3, -2};
+    test_result(linear_search(arr9, 4, -2), 3, "Array with negative numbers and zeros");
 
     return 0;
 }
