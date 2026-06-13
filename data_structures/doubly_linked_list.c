a27ae8374ddaf9bf0297d606fce283203845ede0
Ayman Ali <aymanhashem0001@gmail.com>
2026-06-13 14:10:30 +0300

    coded the insert_dnode function

--- data_structures/doubly_linked_list.c
+++ data_structures/doubly_linked_list.c
@@ -41,7 +41,73 @@ dNode **make_dlinked_list(char arr[], size_t len, dNode *head_and_tail_ptrs[2]) 
 }
 
 dNode *insert_dnode(dNode *head_or_tail, char *direction, dNode *new_node, int pos) {
+    if (strcasecmp(direction, "forward") == 0) {
+        if (pos == 0) {
+            new_node->next = head_or_tail;
+            new_node->prev = NULL;
+            head_or_tail->prev = new_node;
 
+            return new_node;
+        }
+
+        dNode *prev_node = head_or_tail;
+
+        for (int i = 1; prev_node->next != NULL; i++) {
+            if (i == pos) {
+                break;
+            }
+
+            prev_node = prev_node->next;
+        }
+
+        dNode *next_node = prev_node->next;
+
+        prev_node->next = new_node;
+        new_node->prev = prev_node;
+
+        new_node->next = next_node;
+        if (next_node != NULL) {
+            next_node->prev = new_node;
+        }
+
+        return head_or_tail;
+    }
+    else if (strcasecmp(direction, "backward") == 0) {
+        if (pos == 0) {
+            new_node->next = NULL;
+            new_node->prev = head_or_tail;
+            head_or_tail->next = new_node;
+
+            return new_node;
+        }
+
+        dNode *next_node = head_or_tail;
+
+        for (int i = 1; next_node->prev != NULL; i++) {
+            if (i == pos) {
+                break;
+            }
+
+            next_node = next_node->prev;
+        }
+
+        dNode *prev_node = next_node->prev;
+
+        new_node->prev = prev_node;
+        if (prev_node != NULL) {
+            prev_node->next = new_node;
+        }
+
+        new_node->next = next_node;
+        next_node->prev = new_node;
+
+        return head_or_tail;
+    }
+    else {
+        printf("new_node was not inserted.\n");
+        printf("direction string was unrecognized. Make sure you entered 'forward' or 'backward'.\n");
+        return head_or_tail;
+    }
 }
 
 dNode *remove_dnode(dNode *head_or_tail, char *direction, int pos) {
