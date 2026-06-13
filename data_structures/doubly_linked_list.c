6d04437ca77a2faf40d9c807891dbc2d6de5809c
Ayman Ali <aymanhashem0001@gmail.com>
2026-06-13 14:54:14 +0300

    coded the remove_dnode function

--- data_structures/doubly_linked_list.c
+++ data_structures/doubly_linked_list.c
@@ -105,13 +105,85 @@ dNode *insert_dnode(dNode *head_or_tail, char *direction, dNode *new_node, int p
     }
     else {
         printf("new_node was not inserted.\n");
-        printf("direction string was unrecognized. Make sure you entered 'forward' or 'backward'.\n");
+        printf("Direction string was unrecognized. Make sure you entered 'forward' or 'backward' correctly.\n");
         return head_or_tail;
     }
 }
 
 dNode *remove_dnode(dNode *head_or_tail, char *direction, int pos) {
+    if (strcasecmp(direction, "forward") == 0) {
+        if (pos == 0) {
+            dNode *new_head = head_or_tail->next;
+            new_head->prev = NULL;
 
+            free(head_or_tail);
+
+            return new_head;
+        }
+
+        dNode *current_node = head_or_tail->next;
+        dNode *prev_node;
+        dNode *next_node;
+
+        for (int i = 1; current_node != NULL; i++) {
+            prev_node = current_node->prev;
+            next_node = current_node->next;
+
+            if (i == pos) {
+                break;
+            }
+
+            current_node = current_node->next;
+        }
+
+        prev_node->next = next_node;
+        if (next_node != NULL) {
+            next_node->prev = prev_node;
+        }
+
+        free(current_node);
+
+        return head_or_tail;
+    }
+    else if (strcasecmp(direction, "forward") == 0) {
+        if (pos == 0) {
+            dNode *new_tail = head_or_tail->prev;
+            new_tail->next = NULL;
+
+            free(head_or_tail);
+
+            return new_tail;
+        }
+
+        dNode *current_node = head_or_tail->prev;
+        dNode *prev_node;
+        dNode *next_node;
+
+        for (int i = 1; current_node != NULL; i++) {
+            prev_node = current_node->prev;
+            next_node = current_node->next;
+
+            if (i == pos) {
+                break;
+            }
+
+            current_node = current_node->prev;
+        }
+
+        next_node->prev = prev_node;
+        if (prev_node != NULL) {
+            prev_node->next = next_node;
+        }
+
+        free(current_node);
+
+        return head_or_tail;
+    }
+    else {
+        printf("No node was removed.\n");
+        printf("Direction string was unrecognized. Make sure you entered 'forward' or 'backward' correctly.\n");
+        return head_or_tail;
+    }
 }
 
 dNode *search_dlinked_list(dNode *head_or_tail, char *direction, char target) {
