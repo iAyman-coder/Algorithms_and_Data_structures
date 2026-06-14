633ade71cadcef71851ee433d3f4b45332462795
Ayman Ali <aymanhashem0001@gmail.com>
2026-06-14 21:42:57 +0300

    deleted the insert, remove, and delete functions

--- data_structures/doubly_linked_list.c
+++ data_structures/doubly_linked_list.c
@@ -40,162 +40,36 @@ dNode **make_dlinked_list(char arr[], size_t len, dNode *head_and_tail_ptrs[2]) 
     return head_and_tail_ptrs;
 }
 
-dNode *insert_dnode(dNode *head_or_tail, char *direction, size_t len, dNode *new_node, int pos) {
-    if (pos < 0 || pos > len) {
-        printf("Invalid position.\n");
-        printf("Can't insert node at position (%i).\n", pos);
-        return head_or_tail;
-    }
+dNode *insert_new_head() {
 
-    if (strcasecmp(direction, "forward") == 0 && head_or_tail->prev == NULL) {
-        if (pos == 0) {
-            new_node->next = head_or_tail;
-            new_node->prev = NULL;
-            head_or_tail->prev = new_node;
-
-            return new_node;
-        }
-
-        dNode *prev_node = head_or_tail;
-
-        for (int i = 1; prev_node->next != NULL; i++) {
-            if (i == pos) {
-                break;
-            }
-
-            prev_node = prev_node->next;
-        }
-
-        dNode *next_node = prev_node->next;
-
-        prev_node->next = new_node;
-        new_node->prev = prev_node;
-
-        new_node->next = next_node;
-        if (next_node != NULL) {
-            next_node->prev = new_node;
-        }
-
-        return head_or_tail;
-    }
-    else if (strcasecmp(direction, "backward") == 0 && head_or_tail->next == NULL) {
-        if (pos == len) {
-            new_node->next = NULL;
-            new_node->prev = head_or_tail;
-            head_or_tail->next = new_node;
-
-            return new_node;
-        }
-
-        dNode *next_node = head_or_tail;
-
-        for (int i = len - 1; next_node->prev != NULL; i--) {
-            if (i == pos) {
-                break;
-            }
-
-            next_node = next_node->prev;
-        }
-
-        dNode *prev_node = next_node->prev;
-
-        new_node->prev = prev_node;
-        if (prev_node != NULL) {
-            prev_node->next = new_node;
-        }
-
-        new_node->next = next_node;
-        next_node->prev = new_node;
-
-        return head_or_tail;
-    }
-    else {
-        printf("new_node was not inserted.\n");
-        printf("Direction string was unrecognized. Make sure you entered 'forward' or 'backward' correctly.\n");
-        return head_or_tail;
-    }
 }
 
-dNode *remove_dnode(dNode *head_or_tail, char *direction, size_t len, int pos) {
-    if (pos < 0 || pos > len - 1) {
-        printf("Invalid position.\n");
-        printf("Can't remove node at position (%i).\n", pos);
-        return head_or_tail;
-    }
+dNode *insert_new_tail() {
 
-    if (strcasecmp(direction, "forward") == 0 && head_or_tail->prev == NULL) {
-        if (pos == 0) {
-            dNode *new_head = head_or_tail->next;
-            new_head->prev = NULL;
+}
 
-            free(head_or_tail);
+dNode *insert_dnode_after_head() {
 
-            return new_head;
-        }
+}
 
-        dNode *current_node = head_or_tail->next;
-        dNode *prev_node;
-        dNode *next_node;
+dNode *insert_dnode_before_tail() {
 
-        for (int i = 1; current_node != NULL; i++) {
-            prev_node = current_node->prev;
-            next_node = current_node->next;
+}
 
-            if (i == pos) {
-                break;
-            }
+dNode *remove_head() {
 
-            current_node = current_node->next;
-        }
+}
 
-        prev_node->next = next_node;
-        if (next_node != NULL) {
-            next_node->prev = prev_node;
-        }
+dNode *remove_tail() {
 
-        free(current_node);
+}
 
-        return head_or_tail;
-    }
-    else if (strcasecmp(direction, "backward") == 0 && head_or_tail->next == NULL) {
-        if (pos == len - 1) {
-            dNode *new_tail = head_or_tail->prev;
-            new_tail->next = NULL;
+dNode *remove_dnode_after_head() {
 
-            free(head_or_tail);
+}
 
-            return new_tail;
-        }
+dNode *remove_dnode_before_tail() {
 
-        dNode *current_node = head_or_tail->prev;
-        dNode *prev_node;
-        dNode *next_node;
-
-        for (int i = len - 2; current_node != NULL; i--) {
-            prev_node = current_node->prev;
-            next_node = current_node->next;
-
-            if (i == pos) {
-                break;
-            }
-
-            current_node = current_node->prev;
-        }
-
-        next_node->prev = prev_node;
-        if (prev_node != NULL) {
-            prev_node->next = next_node;
-        }
-
-        free(current_node);
-
-        return head_or_tail;
-    }
-    else {
-        printf("No node was removed.\n");
-        printf("Direction string was unrecognized. Make sure you entered 'forward' or 'backward' correctly.\n");
-        return head_or_tail;
-    }
 }
 
 dNode *search_dlinked_list(dNode *head_or_tail, char *direction, char target) {
@@ -223,42 +97,6 @@ dNode *search_dlinked_list(dNode *head_or_tail, char *direction, char target) {
     }
 
     return NULL;
-}
-
-dNode *delete_dlinked_list(dNode *head_or_tail, char *direction) {
-    if (strcasecmp(direction, "forward") == 0 && head_or_tail->prev == NULL) {
-        dNode *current_node = head_or_tail;
-        dNode *next_node;
-
-        while (current_node != NULL) {
-            next_node = current_node->next;
-
-            free(current_node);
-
-            current_node = next_node;
-        }
-
-        return NULL;
-    }
-    else if (strcasecmp(direction, "backward") == 0 && head_or_tail->next == NULL) {
-        dNode *current_node = head_or_tail;
-        dNode *prev_node;
-
-        while (current_node != NULL) {
-            prev_node = current_node->prev;
-
-            free(current_node);
-
-            current_node = prev_node;
-        }
-
-        return NULL;
-    }
-    else {
-        printf("Doubly linked list was not delted.\n");
-        printf("Direction string was unrecognized. Make sure you entered 'forward' or 'backward' correctly.\n");
-        return NULL;
-    }
 }
 
 void print_dlinked_list(dNode *head_or_tail, char *direction, size_t len) {
@@ -315,4 +153,12 @@ size_t len_dlinked_list(dNode *head_or_tail, char *direction) {
     }
 
     return len;
+}
+
+dNode *delete_dlinked_list_forward() {
+
+}
+
+dNode *delete_dlinked_list_backward() {
+
 }
--- data_structures/doubly_linked_list.h
+++ data_structures/doubly_linked_list.h
@@ -27,38 +27,6 @@ typedef struct dNode {
 dNode **make_dlinked_list(char arr[], size_t len, dNode *head_and_tail_ptrs[2]);
 
 /**
- * @brief A function that adds a new node to a doubly linked list at a specific position.
- * 
- * @param head_or_tail: Pointer to the head/tail node of the doubly linked list.
- * @param direction: String that tells the function to go forward or backward.
- * @param new_node: Pointer to the new node that will be inserted.
- * @param pos: An integer that represents the position where the new node will be inserted.
- * 
- * @return Pointer to the head node of the doubly linked list.
- * 
- * @note - The new node must be dynamically allocated via malloc() or calloc().
- * @note - Set the direction as "forward" if you passed the head pointer.
- * @note - Set the direction as "backward" if you passed the tail pointer.
- * @note - First position in the doubly linked list is zero, similar to indexes.
- */
-dNode *insert_dnode(dNode *head_or_tail, char *direction, dNode *new_node, int pos);
-
-/**
- * @brief A function that removes a node from a doubly linked list at a specific position.
- * 
- * @param head_or_tail: Pointer to the head/tail node of the doubly linked list.
- * @param direction: String that tells the function to go forward or backward.
- * @param pos: An integer that represents the position of the node that will be deleted.
- * 
- * @return Pointer to the head node of the doubly linked list.
- * 
- * @note - Set the direction as "forward" if you passed the head pointer.
- * @note - Set the direction as "backward" if you passed the tail pointer.
- * @note - First position in the doubly linked list is zero, similar to indexes.
- */
-dNode *remove_dnode(dNode *head_or_tail, char *direction, int pos);
-
-/**
  * @brief A function that searches a doubly linked list for a specific node that contains specific data.
  * 
  * @param head_or_tail: Pointer to the head/tail node of the doubly linked list.
@@ -71,19 +39,6 @@ dNode *remove_dnode(dNode *head_or_tail, char *direction, int pos);
  * @note - Set the direction as "backward" if you passed the tail pointer.
  */
 dNode *search_dlinked_list(dNode *head_or_tail, char *direction, char target);
-
-/**
- * @brief A function that deletes the entire doubly linked list from memory.
- * 
- * @param head_or_tail: Pointer to the head/tail node of the doubly linked list.
- * @param direction: String that tells the function to go forward or backward.
- * 
- * @return The new memory address for head, which is NULL.
- * 
- * @note - Set the direction as "forward" if you passed the head pointer.
- * @note - Set the direction as "backward" if you passed the tail pointer.
- */
-dNode *delete_dlinked_list(dNode *head_or_tail, char *direction);
 
 
 #endif
